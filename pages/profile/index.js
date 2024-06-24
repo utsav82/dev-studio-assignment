498d566b1643ff9c67e098950e7f4ba6dcf82afd
utsav82 <utsa.yadav.94@gmail.com>
2024-06-24 14:49:15 +0530

    updated profile

--- pages/profile/index.js
+++ pages/profile/index.js
@@ -3,7 +3,7 @@ import Link from "next/link";
 
 const ProfilePage = () => {
   return (
-    <div className="h-[screen] flex flex-col items-center p-5 space-y-10 relative">
+    <div className="h-screen flex flex-col items-center p-5 space-y-10 relative">
       <div className=" flex justify-center w-full">
         <Link href="/" className="absolute left-5">
           {" "}
@@ -22,8 +22,8 @@ const ProfilePage = () => {
             fill="none"
             stroke="white"
             stroke-width="2"
-            stroke-linecap="round"
-            stroke-linejoin="round"
+            strokeLinecap="round"
+            strokeLinejoin="round"
           >
             <path d="M14.5 4h-5L7 7H4a2 2 0 0 0-2 2v9a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V9a2 2 0 0 0-2-2h-3l-2.5-3z" />
             <circle cx="12" cy="13" r="3" />
@@ -40,10 +40,17 @@ const ProfilePage = () => {
           className="mt-1 block w-full border border-gray-300 rounded-md h-[60px] pl-5"
         />
 
-        <div className="flex shadow-sm border border-gray-300 rounded-md">
-          <span className="flex items-center p-2 bg-gray-100 border-r border-gray-300 h-[60px]">
-            +91
-          </span>
+        <div className="relative flex items-center w-full border border-gray-300 rounded-md px-4 h-[60px]">
+          <img
+            src="https://upload.wikimedia.org/wikipedia/en/thumb/4/41/Flag_of_India.svg/383px-Flag_of_India.svg.png"
+            alt="India Flag"
+            className="mr-2 h-6 rounded-md"
+          />
+
+          <div className="border-r border-gray-300 h-4 mr-2"></div>
+
+          <span className=" mr-2">+91</span>
+
           <input
             type="text"
             id="mobileNumber"
@@ -72,7 +79,7 @@ const ProfilePage = () => {
         <select
           id="city"
           name="city"
-          defaultValue="New York"
+          defaultValue="City"
           className="mt-1 block w-full bg-white border border-gray-300 rounded-md px-4 h-[60px] text-gray-500"
         >
           <option disabled>City</option>
@@ -84,7 +91,7 @@ const ProfilePage = () => {
         <select
           id="district"
           name="district"
-          defaultValue="Manhattan"
+          defaultValue="District"
           className="mt-1 block w-full border bg-white border-gray-300 rounded-md px-4 h-[60px] text-gray-500"
         >
           <option disabled>District</option>
@@ -95,10 +102,10 @@ const ProfilePage = () => {
       </div>
 
       <div className="flex space-x-4 w-full">
-        <button className="flex-1 h-[56px] py-2 px-4 border border-gray-300 rounded-md">
+        <button className="flex-1 h-[56px] py-2 px-4 border border-[#008955] rounded-md">
           Cancel
         </button>
-        <button className="flex-1 h-[56px] py-2 px-4 border border-primary rounded-md bg-primary text-white">
+        <button className="flex-1 h-[56px] py-2 px-4 border border-[#008955] rounded-md bg-primary text-white">
           Save
         </button>
       </div>
